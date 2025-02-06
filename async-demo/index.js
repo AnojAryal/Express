ab2a90c2cdc22626d12cd3fdfb8c08839ca174f8
AnojAryal <anoj1810@gmail.com>
2025-02-06 15:14:56 +0545

    using callbacks

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,6 +1,30 @@
 console.log("Before");
-//asyncronous or non blocking function
-setTimeout(() => {
-    console.log("Reading a user from a database...");
-}, 2000);
+const user = getUser(1,  (user)=> {
+    console.log("User", user);
+
+    //get the repositories
+    getRepositories(user.gitHubUsername,  (repos)=> {
+        console.log("Repos", repos);
+    });
+});
+
 console.log("After");
+
+
+//asyncronous function
+function getUser(id, callback) {
+    setTimeout(() => {
+        console.log("Reading a user from a database...");
+        //using callback function to return the user
+        callback({ id: id, gitHubUsername: 'anoj' });
+    }, 2000);
+
+}
+
+//asyncronous function to get the repositories with callback
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        console.log("Calling GitHub API...");
+        callback(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+}

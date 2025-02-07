6c26999403512d0bed52695885be82e06a51f8d2
AnojAryal <anoj1810@gmail.com>
2025-02-07 11:22:01 +0545

    async and await

---
+++ async-demo/async.js
@@ -0,0 +1,47 @@
+console.log("Before");
+
+async function displayCommits() {
+    try {
+        const user = await getUser(1);
+        const repos = await getRepositories(user.gitHubUsername);
+        const commits = await getCommits(repos);
+        console.log(commits);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+displayCommits();
+
+console.log("After");
+
+
+// Asynchronous function to get the user
+function getUser(id) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Reading a user from a database...");
+            resolve({ id: id, gitHubUsername: 'anoj' });
+        }, 2000);
+    });
+}
+
+// Asynchronous function to get repositories
+function getRepositories(username) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Calling GitHub API...");
+            resolve(['repo1', 'repo2', 'repo3']);
+        }, 2000);
+    });
+}
+
+// Asynchronous function to get commits
+function getCommits(repos) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Fetching commits...");
+            resolve(['commit1', 'commit2', 'commit3']);
+        }, 2000);
+    });
+}
